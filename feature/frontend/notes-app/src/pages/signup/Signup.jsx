8acde7e233bd8d4be860da985807473babf02a44
MonaRose1 <maimoonanaumani2@gmail.com>
2025-09-21 21:20:53 +0500

    Login and signup page front end complete

--- feature/frontend/notes-app/src/pages/signup/Signup.jsx
+++ feature/frontend/notes-app/src/pages/signup/Signup.jsx
@@ -1,16 +1,33 @@
-import React from 'react';
+import React,{ useState } from 'react';
+
 const Signup = () => {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    // eslint-disable-next-line no-unused-vars
+    const [error, setError] = useState(null);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setError(null);
+};
+
+
+
   return (
     <>
-    <div>
-        <div><p>Signup</p></div>
-        <div><input type = 'text' placeholder = 'username' /></div>
-        <div><input type = 'text' placeholder = 'email' /></div>
-        <div><input type = 'text' placeholder = 'password' /></div>
-        <div><button>Submit</button></div>
-        <div><p>Already have an account? <a href = '/login'> Login</a></p></div>
+    <div className='flex justify-center items-center mt-28 '>
+        <div className="w-96 border rounded bg-white px-7 py-10">
+            <form onSubmit={handleSubmit}>
+                <h2 className="text-2xl font-bold mb-5 ">Signup</h2>
+                <input type = 'text' placeholder = 'username' className="input-box" value={username} onChange={(e) => setUsername (e.target.value)}/>
+            <input type = 'text' placeholder = 'email' className="input-box" value={email} onChange={(e) => setEmail (e.target.value)}/>
+                <input type = 'text' placeholder = 'password' className="input-box" value={password} onChange={(e) => setPassword (e.target.value)}/>
+                <button className="btn-primary">Submit</button>
+                <p className= "text-gray-600 text-sm mt-5">Already have an account? <a href = '/login'> Log in</a></p>
 
-    
+            </form>
+        </div>
     </div>
     </>
   );
