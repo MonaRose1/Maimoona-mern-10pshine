893058be0582bd4289708fca61f091aa96fcc1e4
MonaRose1 <maimoonanaumani2@gmail.com>
2025-10-07 00:58:13 +0500

    feat(editor): add NoteEditor with contentEditable, load/save to /api/notes, route /note/:id

---
+++ feature/frontend/notes-app/src/App.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/home/Home.jsx"; 
+import Login from "./pages/login/Login.jsx";
+import Signup from "./pages/signup/Signup.jsx";
+import NoteEditor from "./components/NoteEditor.jsx";
+import "./App.css";
+const ProtectedRoute = ({ children }) => {
+  const token = typeof window !== 'undefined' ? localStorage.getItem('token') : null;
+  if (!token) {
+    return <Navigate to="/login" replace />;
+  }
+  return children;
+};
+
+const routes = (
+  <Routes>
+    <Route path="/" element={<Navigate to="/home" replace />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/signup" element={<Signup />} />
+    <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+    <Route path="/note/:id" element={<ProtectedRoute><NoteEditor /></ProtectedRoute>} />
+  </Routes>
+);
+
+const App = () => {
+  return (
+    <div>
+      {routes}
+    
+    </div>
+  );
+};
+
+export default App;
