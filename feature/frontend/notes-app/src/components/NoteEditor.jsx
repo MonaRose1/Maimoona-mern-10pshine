169fa314b8b8045e1db5260d66bddecc2b2903fb
MonaRose1 <maimoonanaumani2@gmail.com>
2025-11-04 20:50:05 +0500

    Merge files from feature/frontend/tests-jest branch

--- feature/frontend/notes-app/src/components/NoteEditor.jsx
+++ feature/frontend/notes-app/src/components/NoteEditor.jsx
@@ -4,7 +4,7 @@ import { apiRequest } from '../utils/helper';
 import Loading from './Loading.jsx';
 import ErrorMessage from './ErrorMessage.jsx';
 
-export const NoteEditor = () => {
+const NoteEditor = () => {
   const { id } = useParams();
   const navigate = useNavigate();
   const titleRef = useRef(null);
